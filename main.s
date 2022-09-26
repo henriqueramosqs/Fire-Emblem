e29cf5f3fe6417e34e3ed5fb5de98c1fc370171f
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2022-09-26 11:14:06 -0300

    primeiro commit

---
+++ main.s
@@ -0,0 +1,346 @@
+.data
+	.include "images_data/Bush.data"
+	.include "images_data/Grass.data"
+	.include "maps/Mapa_fase1.data"
+	
+	frame_zero: .word 0xFF000000
+	frame_one:  .word 0xFF100000
+
+	#necessario para printString
+	LabelTabChar:
+		.word 	0x00000000, 0x00000000, 0x10101010, 0x00100010, 0x00002828, 0x00000000, 0x28FE2828, 0x002828FE, 
+		0x38503C10, 0x00107814, 0x10686400, 0x00004C2C, 0x28102818, 0x003A4446, 0x00001010, 0x00000000, 
+		0x20201008, 0x00081020, 0x08081020, 0x00201008, 0x38549210, 0x00109254, 0xFE101010, 0x00101010, 
+		0x00000000, 0x10081818, 0xFE000000, 0x00000000, 0x00000000, 0x18180000, 0x10080402, 0x00804020, 
+		0x54444438, 0x00384444, 0x10103010, 0x00381010, 0x08044438, 0x007C2010, 0x18044438, 0x00384404, 
+		0x7C482818, 0x001C0808, 0x7840407C, 0x00384404, 0x78404438, 0x00384444, 0x1008047C, 0x00202020, 
+		0x38444438, 0x00384444, 0x3C444438, 0x00384404, 0x00181800, 0x00001818, 0x00181800, 0x10081818, 
+		0x20100804, 0x00040810, 0x00FE0000, 0x000000FE, 0x04081020, 0x00201008, 0x08044438, 0x00100010, 
+		0x545C4438, 0x0038405C, 0x7C444438, 0x00444444, 0x78444478, 0x00784444, 0x40404438, 0x00384440,
+		0x44444478, 0x00784444, 0x7840407C, 0x007C4040, 0x7C40407C, 0x00404040, 0x5C404438, 0x00384444, 
+		0x7C444444, 0x00444444, 0x10101038, 0x00381010, 0x0808081C, 0x00304848, 0x70484444, 0x00444448, 
+		0x20202020, 0x003C2020, 0x92AAC682, 0x00828282, 0x54546444, 0x0044444C, 0x44444438, 0x00384444, 
+		0x38242438, 0x00202020, 0x44444438, 0x0C384444, 0x78444478, 0x00444850, 0x38404438, 0x00384404, 
+		0x1010107C, 0x00101010, 0x44444444, 0x00384444, 0x28444444, 0x00101028, 0x54828282, 0x00282854, 
+		0x10284444, 0x00444428, 0x10284444, 0x00101010, 0x1008047C, 0x007C4020, 0x20202038, 0x00382020, 
+		0x10204080, 0x00020408, 0x08080838, 0x00380808, 0x00442810, 0x00000000, 0x00000000, 0xFE000000, 
+		0x00000810, 0x00000000, 0x3C043800, 0x003A4444, 0x24382020, 0x00582424, 0x201C0000, 0x001C2020, 
+		0x48380808, 0x00344848, 0x44380000, 0x0038407C, 0x70202418, 0x00202020, 0x443A0000, 0x38043C44, 
+		0x64584040, 0x00444444, 0x10001000, 0x00101010, 0x10001000, 0x60101010, 0x28242020, 0x00242830, 
+		0x08080818, 0x00080808, 0x49B60000, 0x00414149, 0x24580000, 0x00242424, 0x44380000, 0x00384444, 
+		0x24580000, 0x20203824, 0x48340000, 0x08083848, 0x302C0000, 0x00202020, 0x201C0000, 0x00380418, 
+		0x10381000, 0x00101010, 0x48480000, 0x00344848, 0x44440000, 0x00102844, 0x82820000, 0x0044AA92, 
+		0x28440000, 0x00442810, 0x24240000, 0x38041C24, 0x043C0000, 0x003C1008, 0x2010100C, 0x000C1010, 
+		0x10101010, 0x00101010, 0x04080830, 0x00300808, 0x92600000, 0x0000000C, 0x243C1818, 0xA55A7E3C, 
+		0x99FF5A81, 0x99663CFF, 0x10280000, 0x00000028, 0x10081020, 0x00081020
+
+.text
+
+	li a0,1
+StartLevel:	#recebe em a0 o número da fase, efetua os proicedimentos necessários
+	#<procedimento_de_rodar_história>
+	jal getLevelMap
+	lw a1, frame_zero
+	jal printMap
+		
+		
+getLevelMap: # recebe em a0 numero da fase, retorna em a0 o endereco do mapa ou -1 se mapa da fase nao foi encontrado
+	 li t0,1
+	 beq a0, t0,mapLevel1  #checa se a0=1
+	 addi t0,t0,1
+	 beq a0,t0,mapLevel2 #checa se a0=2
+	 addi t0,t0,1
+	 beq a0,t0,mapLevel3 #checa se a0=3
+	 addi t0,t0,1
+	 beq a0,t0,mapLevel4 #checa se a0=4
+	 addi t0,t0,1
+	 beq a0,t0,mapLevel5 #checa se a0=5
+	 li a0,-1
+	 ret
+	 
+mapLevel1:
+	la a0,Mapa_fase1
+	ret
+mapLevel2:
+	#la a0,Mapa_fase2
+	ret
+mapLevel3:	
+	#la a0,Mapa_fase3
+	ret
+mapLevel4:
+	#la a0,Mapa_fase4
+	ret
+mapLevel5:
+	#la a0,Mapa_fase5
+	ret
+	
+printMap:		# :void, recebe em a0  o endereco do arquivo do mapa e em a1 o endereco da frame, pinta o mapa na tela
+			# sobresreceve s1,s2,s3 e s4
+	addi sp,sp, -16
+	sw s1,0(sp)
+	sw s2,4(sp)
+	sw s3,8(sp)
+	sw s4,12(sp)
+	
+	mv s1,a0	# s1 = endereco inicial do arquivo de mapa
+
+	#addi s1,s1,260		# para debugging
+
+	li s2, 280	# s2 = quantidade de bytes por arquivos de mapa
+	add s2,s2,a0 	# s2 =  endereco final do arquivo de mapa
+	li s3, 0	# s3 = contador utilizado para posições
+	li s4,320	# s4 = numero utilizado em contas 
+	mv a3, a1	# coloca em a3 o endereco da frame para ser utilizado peloi drawImage 	
+
+printMapLoop: 
+	beq s1,s2,printMapEnd 	# checa condicao de parada
+	mv a0,s1	
+	lb a0,0(a0)
+	jal getTile 		# armazena endereco do tile em a0
+	
+	rem a1,s3,s4		# a1 = coord_x
+	div a2,s3,s4		
+	slli a2,a2,4		# a2 = coord_y
+	
+	jal drawImage		# pinta Tile
+	
+	addi s1,s1,1
+	addi s3,s3,16
+	j printMapLoop
+
+printMapEnd:
+	ret
+	
+
+getTile: #recebe em a0 o código do tile, retorna em a0 o endereço do tile ou -1 se nao encontrado
+	li t0,1
+	beq a0,t0,grassTile
+	addi t0,t0,1
+	beq a0,t0,bushTile
+	li a0,-1
+	ret
+grassTile:
+	la a0, Grass
+	ret
+bushTile:
+	la a0,Bush
+	ret
+	
+
+
+
+readKeyBlocking:
+# Pausa o programa e espera que o usu?rio pressione uma tecla para continuar
+#
+# caractere pressionado -> a0
+	
+	li t1, 0xFF200000	# Carrega o endereco de status do KDMMIO
+rkb_loop:
+	lw t0, 0(t1)		# Carrega o status do teclado
+	andi t0, t0, 0x0001	# Mascara o bit menos significativo.
+	beq t0, zero, rkb_loop	# Se nao houver tecla pressionada repete ate que o usuario aperte algo
+	lw a0, 4(t1)		# Carrega o caractere que foi pressionado
+	sw a0, 12(t1)		# Escreve o caractere lido no display 
+	ret	
+
+endProgram:
+	li a7, 10
+	ecall
+
+
+
+readKeyNonBlocking:
+# Verifica se o usu?rio pressionou uma tecla, se não continua o programa, se
+# sim, retorna o caractere pressionado
+#
+# caractere -> a0
+	
+	li t1, 0xFF200000	# Carrega o endereço de status do KDMMIO
+	lw t0, 0(t1)		# Carrega o status do teclado
+	andi t0, t0, 0x0001	# Mascara o bit menos significativo
+	beq t0, zero, rknb_end	# Se não houver tecla pressionada continua
+	lw t2, 4(t1)		# Carrega o caractere que foi pressionado
+	lw a0, 4(t1)		# Carrega em a0 (vari?vel de retorno) o caractere
+	sw t2, 12(t1)		# Escreve o caractere lido no display
+rknb_end:
+	ret			#retorna 
+	
+	
+	
+drawImage:	# a0= endereço da imagem, a1= coord_x, a2=coord_y, a3=frame
+
+	lw t0,0(a0)	# t0 armazena largura da imagem
+	lw t1,4(a0)	# t1 armazena altura da imagem
+
+	li t2,320
+	mul t2,a2,t2
+	add t2,t2,a1
+	add t2,t2,a3	# t2 armazena endereco inicial/atual da área de pintura
+	
+	li t3,320
+	mul t3,t3,t1
+	add t3,t3,t0
+	add t3,t3,t2 	# t3 armazena enderço final da área de pintura
+	addi t3,t3,-320
+		
+	addi a0,a0,8	# muda a0 para o endereço inicial das cores
+	li t5,0 	# t5 é um marcador de passos à direita
+	li t4, 0xC7
+
+drawLoop:
+	beq t2,t3,finishDraw # se endereço atual = endereço final, finaliza pintura
+	bne t5,t0, keepDraw
+	
+	addi t2,t2,320		#muda t2 para primeira posição da linha debaixo
+	sub t2,t2,t0	
+	add t5,zero,zero
+keepDraw:
+	lb t6,(a0) # t6 carrega coloração do pixel
+	beq t4,t6,transparent
+	sb t6,0(t2)
+transparent:
+	addi t2,t2,1  # muda para póximo endereço
+	addi t5,t5,1  #passa o contador de passos para a direita
+	addi a0,a0,1
+	j drawLoop
+finishDraw:
+	ret
+	
+
+Blackout:			# :void, receve em a0 = endereco da frame, pinta a frame de preto
+	li t1,19200
+	mv t2,a0		#endereço base da frame
+	li t3,0
+	
+BlackoutLoop:
+	beq t0,t1,BlackoutEnd  #se escever a quantidade de words suficientes, finaliza o loopp
+	slli t4,t0,2		#t4 = 4* indice_da_word
+	add t4,t4,t2		#t4 = endereço_base_da_frame + 4* indice_da_word
+	sw t3, (t4)		
+	addi t0,t0,1
+	j BlackoutLoop
+	
+BlackoutEnd:
+	ret
+	
+
+printString:	
+		addi	sp, sp, -8			# aloca espaco
+    		sw	ra, 0(sp)			# salva ra
+    		sw	s0, 4(sp)			# salva s0
+    		mv	s0, a0              		# s0 = endereco do caractere na string
+
+loopprintString:lb	a0, 0(s0)                 	# le em a0 o caracter a ser impresso
+
+    		beq     a0, zero, fimloopprintString	# string ASCIIZ termina com NULL
+
+    		jal     printChar       		# imprime char
+    		
+		addi    a1, a1, 8                 	# incrementa a coluna
+		li 	t6, 313		
+		blt	a1, t6, NaoPulaLinha	    	# se ainda tiver lugar na linha
+    		addi    a2, a2, 8                 	# incrementa a linha
+    		mv    	a1, zero			# volta a coluna zero
+
+NaoPulaLinha:	addi    s0, s0, 1			# proximo caractere
+    		j       loopprintString       		# volta ao loop
+
+fimloopprintString:	lw      ra, 0(sp)    		# recupera ra
+			lw 	s0, 0(sp)		# recupera s0 original
+    			addi    sp, sp, 8		# libera espaco
+fimprintString:	ret      	    			# retorna
+
+
+#########################################################
+#  PrintChar                                            #
+#  a0 = char(ASCII)                                     #
+#  a1 = x                                               #
+#  a2 = y                                               #
+#  a3 = cores (0x0000bbff) 	b = fundo, f = frente	#
+#  a4 = frame (0 ou 1)					#
+#########################################################
+#   t0 = i                                             #
+#   t1 = j                                             #
+#   t2 = endereco do char na memoria                   #
+#   t3 = metade do char (2a e depois 1a)               #
+#   t4 = endereco para impressao                       #
+#   t5 = background color                              #
+#   t6 = foreground color                              #
+#########################################################
+#	t9 foi convertido para s9 pois nao ha registradores temporarios sobrando dentro desta funcao
+
+
+printChar:	li 	t4, 0xFF	# t4 temporario
+		slli 	t4, t4, 8	# t4 = 0x0000FF00 (no RARS, nao podemos fazer diretamente "andi rd, rs1, 0xFF00")
+		and    	t5, a3, t4   	# t5 obtem cor de fundo
+    		srli	t5, t5, 8	# numero da cor de fundo
+		andi   	t6, a3, 0xFF    # t6 obtem cor de frente
+
+		li 	tp, ' '
+		blt 	a0, tp, printChar.NAOIMPRIMIVEL	# ascii menor que 32 nao eh imprimivel
+		li 	tp, '~'
+		bgt	a0, tp, printChar.NAOIMPRIMIVEL	# ascii Maior que 126  nao eh imprimivel
+    		j       printChar.IMPRIMIVEL
+    
+printChar.NAOIMPRIMIVEL: li      a0, 32		# Imprime espaco
+
+printChar.IMPRIMIVEL:	li	tp, 320		# Num colunas 320
+			mul t4,tp,a2
+			j printChar.mul1d
+printChar.mul1:		mul     t4, tp, a2			# multiplica a2x320  t4 = coordenada y
+printChar.mul1d:	add     t4, t4, a1               	# t4 = 320*y + x
+			addi    t4, t4, 7                 	# t4 = 320*y + (x+7)
+			li      tp, 0xFF000000          	# Endereco de inicio da memoria VGA0
+			beq 	a4, zero, printChar.PULAFRAME		# Verifica qual o frame a ser usado em a4
+			li      tp, 0xFF100000          	# Endereco de inicio da memoria VGA1
+printChar.PULAFRAME:	add     t4, t4, tp               	# t4 = endereco de impressao do ultimo pixel da primeira linha do char
+			addi    t2, a0, -32               	# indice do char na memoria
+			slli    t2, t2, 3                 	# offset em bytes em relacao ao endereco inicial
+			la      t3, LabelTabChar		# endereco dos caracteres na memoria
+			add     t2, t2, t3               	# endereco do caractere na memoria
+			lw      t3, 0(t2)                 	# carrega a primeira word do char
+			li 	t0, 4				# i=4
+
+printChar.forChar1I:	beq     t0, zero, printChar.endForChar1I # if(i == 0) end for i
+    			addi    t1, zero, 8               	# j = 8
+
+printChar.forChar1J:	beq     t1, zero, printChar.endForChar1J # if(j == 0) end for j
+        		andi    s9, t3, 0x001			# primeiro bit do caracter
+        		srli    t3, t3, 1             		# retira o primeiro bit
+        		beq     s9, zero, printChar.printCharPixelbg1	# pixel eh fundo?
+        		sb      t6, 0(t4)             		# imprime pixel com cor de frente
+        		j       printChar.endCharPixel1
+printChar.printCharPixelbg1:	sb      t5, 0(t4)                # imprime pixel com cor de fundo
+printChar.endCharPixel1: addi    t1, t1, -1                	# j--
+    			addi    t4, t4, -1                	# t4 aponta um pixel para a esquerda
+    			j       printChar.forChar1J		# vollta novo pixel
+
+printChar.endForChar1J: addi    t0, t0, -1 		# i--
+    			addi    t4, t4, 328           	# 2**12 + 8
+    			j       printChar.forChar1I	# volta ao loop
+
+printChar.endForChar1I:	lw      t3, 4(t2)           	# carrega a segunda word do char
+			li 	t0, 4			# i = 4
+printChar.forChar2I:    beq     t0, zero, printChar.endForChar2I  # if(i == 0) end for i
+    			addi    t1, zero, 8             # j = 8
+
+printChar.forChar2J:	beq	t1, zero, printChar.endForChar2J # if(j == 0) end for j
+        		andi    s9, t3, 0x001	    		# pixel a ser impresso
+        		srli    t3, t3, 1                 	# desloca para o proximo
+        		beq     s9, zero, printChar.printCharPixelbg2 # pixel eh fundo?
+        		sb      t6, 0(t4)			# imprime cor frente
+        		j       printChar.endCharPixel2		# volta ao loop
+
+printChar.printCharPixelbg2:	sb      t5, 0(t4)		# imprime cor de fundo
+
+printChar.endCharPixel2:	addi    t1, t1, -1		# j--
+    				addi    t4, t4, -1              # t4 aponta um pixel para a esquerda
+    				j       printChar.forChar2J
+
+printChar.endForChar2J:	addi	t0, t0, -1 		# i--
+    			addi    t4, t4, 328		#
+    			j       printChar.forChar2I	# volta ao loop
+
+printChar.endForChar2I:	ret	
+	
+	
